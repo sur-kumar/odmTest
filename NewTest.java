a0ca2d1a3b535e7d462232a207cae890c0942e20
sur-kumar <55438864+sur-kumar@users.noreply.github.com>
2019-09-17 15:50:18 +0530

    Create NewTest.java

---
+++ NewTest.java
@@ -0,0 +1,8 @@
+public class Address
+{
+    /** Represents the first line of the address */
+    private String  firstLine;
+
+    /** Represents the last line of the address */
+    private String  lastLine;
+}
